8016cfaae3b01a7cdb0f995723f3d9a1026bfc40
JPolm <jstnplm@gmail.com>
2018-11-07 20:11:51 -0800

    dayTwo done

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+//String
+// var name = "justin";
+//
+// //String Object
+// var nameTwo = new String("Reckas")
+// //number
+// var luckyNumber = 42;
+// //boolean
+// var boollean = true;
+// //Undefined
+// //console.log(typeof(name));
+// //null
+// //console.log(typeof(null));
+//
+// //console.log(name == nameTwo);
+
+// const  name = "Justin"
+//
+// console.log(name);
+// {
+//   const name = "Norma";
+//   console.log(name)
+// }
+// console.log(name);
+// const greeting = "Hello my name is: ";
+// const name = "Bob"
+
+
+// const date = new Date("11/20/97");
+// console.log(date);
+//
+//
+// const str = "qeweqweqweqew";
+// console.log(str.lenght)
+//
+// cosnt num1 = new Number(42);
+// const num2 = 42;
+
+// const array = [1,2,3];
+// const array2 = new Array(1,3);
+// const arrayMixed = ["Justin", 20, true]
+// console.log(arrayMixed);
+//
+// const obj = {
+//   name: "Justin",
+//   age: 20
+// };
+//
+// console.log(obj);
+//alert("Dange Will Robinson!!!");
+// const response = confirm("Are you sure?");
+// console.log(response);
+const favNum = prompt("What is your favorite number?");
+const realNum = parseInt(favNum);
+console.log(realNum);
